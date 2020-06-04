03057430915b27f10175933c858db6181c144298
Sadiqueali042 <Sadiqueali042@gmail.com>
2020-06-04 06:53:49 +0500

    iniatial Reat app project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+Hello World my name is sadiq Khan
+    </div>
+  );
+}
+
+export default App;
